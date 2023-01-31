c21aedd46d9e477bfe8e8a00e6cb3277c1da433c
jpromanonet <programming@jpromano.net>
2023-01-30 22:36:57 -0300

    Ejercicio 03

--- Ejercicio_03/index.js
+++ Ejercicio_03/index.js
@@ -1,57 +1,61 @@
-const fs = require('fs');
+const fs = require('fs')
 
-// Load JSON object from file
+// Cargar el objeto JSON
 fs.readFile('holding.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  
-  let obj = JSON.parse(data);
+    if (err) throw err;
 
-  // Normalize JSON object
-  let normalizedObj = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (typeof value === 'object') {
-      let subObj = {};
-      for (const [subKey, subValue] of Object.entries(value)) {
-        subObj[`${key}_${subKey}`] = subValue;
-      }
-      Object.assign(normalizedObj, subObj);
-    } else {
-      normalizedObj[key] = value;
+    let obj = JSON.parse(data);
+
+    // Normalizar el objeto
+    let normalizedObj =  {};
+    for (const [key, value] of Object.entries(obj)){
+        if(typeof value === 'Object') {
+                let subObj = {};
+            for (const [subKey, subValue] of Object.entries(value)){
+                subObj[`${key}_${subKey}`] = subValue;
+            }
+            Object.assign(normalizedObj, subObj);
+        } else {
+            normalizedObj = value;
+        }
     }
-  }
 
-  // Print normalized object
-  console.log(normalizedObj);
+    // Imprimir el objeto normalizado
+    console.log(normalizedObj)
 
-  // Denormalize JSON object
-  let denormalizedObj = {};
-  for (const [key, value] of Object.entries(normalizedObj)) {
-    let parts = key.split('_');
-    if (parts.length > 1) {
-      let subObjKey = parts.slice(0, -1).join('_');
-      let subKey = parts[parts.length - 1];
-      if (subObjKey in denormalizedObj) {
-        denormalizedObj[subObjKey][subKey] = value;
-      } else {
-        denormalizedObj[subObjKey] = { [subKey]: value };
-      }
-    } else {
-      denormalizedObj[key] = value;
+    // Desnormalizar el objeto
+    let desnormalizarObj = {};
+    for(const [key, value] of Object.entries(normalizedObj)){
+        let parts = key.split('_');
+        if(parts.length > 1){
+            let subObjKey = part.slice(0, -1).join('_');
+            let subKey = parts[parts.length -1];
+            if(subObjKey in desnormalizarObj) {
+                desnormalizarObj[subObjKey][subKey] = value;
+            } else {
+                desnormalizarObj[subObjKey] = { [subKey]: value };
+            } 
+        } else {
+            desnormalizarObj[key] = value
+        }   
     }
-  }
 
-  // Print denormalized object
-  console.log(denormalizedObj);
+    // Imprimir el objeto desnormalizado en pantalla
+    console.log(desnormalizarObj)
 
-  // Print length of original, normalized, and denormalized objects
-  let originalLength = JSON.stringify(obj).length;
-  let normalizedLength = JSON.stringify(normalizedObj).length;
-  let denormalizedLength = JSON.stringify(denormalizedObj).length;
-  console.log(`Original length: ${originalLength}`);
-  console.log(`Normalized length: ${normalizedLength}`);
-  console.log(`Denormalized length: ${denormalizedLength}`);
+    // Imprimir el largo del objeto original, normalizado y desnormalizado
 
-  // Print compression percentage
-  let compressionPercentage = 100 - (normalizedLength / originalLength * 100);
-  console.log(`Compression percentage: ${compressionPercentage.toFixed(2)}%`);
-});
+    // Contamos el largo de nuestros objetos
+    let originalLength = JSON.stringify(obj).length;
+    let normalizedLength = JSON.stringify(normalizedObj).length;
+    let desnormalizedLegth = JSON.stringify(desnormalizarObj).length;
+
+    // Mostramos en consola
+    console.log('Largo original: ', originalLength);
+    console.log('Largo normalizado: ', normalizedLength);
+    console.log('Largo desnormalizado: ', desnormalizedLegth)
+
+    // Mostrar el porcentaje de compresión
+    let compressionPercentage = 100 - (normalizedLength / originalLength * 100);
+    console.log('Porcejante de compresión: ', compressionPercentage.toFixed(2));
+})
